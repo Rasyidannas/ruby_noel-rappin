8217ff1e1c6830bf3b368c72791503df2c63f33a
rasyidannas <rasshit.dsgn@gmail.com>
2024-12-25 15:04:16 +0700

    Chapter 9 - Using Stimulus to Manage Forms

--- app/packs/controllers/search_controller.ts
+++ app/packs/controllers/search_controller.ts
@@ -5,7 +5,39 @@ export default class SearchController extends Controller {
   static targets = ["form", "input"]
   formTarget: HTMLFormElement
 
+  resetOnOutsideClick(event: Event): void {
+    if (!this.element.contains(event.target as HTMLElement)) {
+      this.reset()
+    }
+  }
+  
+  reset(): void {
+    this.resultsTarget.classList.add("hidden")
+    this.resultsTarget.innerText = ""
+    this.inputTarget.value=""
+  }
+
+  basicSubmit(): void {
+    if (this.inputTarget.value === "") {
+      this.reset()
+    } else {
+      this.formTarget.requestSubmit()
+    }
+  }
+
   submit(): void {
     this.formTarget.requestSubmit()
+  }
+
+  debounce(functionToDebounce, wait = 300) {
+    let timeoutId = null
+
+    return (...args) => {
+      clearTimeout(timeoutId)
+      timeoutId = setTimeout(() => {
+        timeoutId = null
+        functionToDebounce(...args)
+      }, wait);
+    }
   }
 }
